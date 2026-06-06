21ae765b2f1a1501d558cf8a9d6043216b809833
Adam Bąk <Baczekadam@wp.pl>
2026-06-06 20:44:28 +0200

    Adding changing color functionality to the button
    Creating new function that returns random color value with f-string, and adding change of color in button

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QLabel, QPushButton
 import sys
+import random
 
 # This is a simple PySide6 application
 # Main Window is a class that inherits from QMainWindow
@@ -23,39 +24,45 @@ class MainWindow(QMainWindow):
         self.label = QLabel("Click the button")
         self.button_hello = QPushButton("Say Hello")
         self.button_clear = QPushButton("Clear Text")
-        self.button_test_CLI = QPushButton("CLI Test")
+        self.button_test_CLI = QPushButton("CLI Test & Color change")
 
         self.layout.addWidget(self.label)
         self.layout.addWidget(self.button_hello)
         self.layout.addWidget(self.button_clear)
         self.layout.addWidget(self.button_test_CLI)
         
-
     def _setup_events(self):
         #send signal when the button is clicked
-        self.button_test_CLI.clicked.connect(self.test_CLI)
         self.button_hello.clicked.connect(self.say_hello)
         self.button_clear.clicked.connect(self.clear_text)
+        self.button_test_CLI.clicked.connect(self.change_btn_color)
         
         self.central_widget.setLayout(self.layout)
         self.setCentralWidget(self.central_widget)
-        
-
     
-    #Signal test
-    def test_CLI(self):
-        print("Signal was sent from the button")
+
     def say_hello(self):
         self.label.setText("Hello from function")
     def clear_text(self):
         self.label.setText("")
+    def change_btn_color(self):
+        print("Button Color changed")
+        self.button_test_CLI.setStyleSheet(f"background-color: {self.randomize_color()};")
+
+    
+    def randomize_color(self):
+        r = random.randint(0, 255)
+        g = random.randint(0, 255)
+        b = random.randint(0, 255)
+        return f"rgb({r}, {g}, {b})"
+
         
 #this function will load ctyle from .qss file
 def load_stylesheet():                      
     with open("style.qss", "r") as style_file:
         qss = style_file.read()
         app.setStyleSheet(qss)
-    
+
 
 # The main block checks if the script is run directly (not imported as a module)
 if __name__ == "__main__":
